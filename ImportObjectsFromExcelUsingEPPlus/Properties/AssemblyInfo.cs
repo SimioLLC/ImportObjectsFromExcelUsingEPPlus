fe122519f354094789449bb919840ca728e67b6a
batpox <dan.houck@batpox.com>
2022-07-19 10:50:54 -0400

    Updated to last LGPL licensed EPPlus. Update to net 4.7.2 and tested on Simio 14.240

--- ImportObjectsFromExcelUsingEPPlus/Properties/AssemblyInfo.cs
+++ ImportObjectsFromExcelUsingEPPlus/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Simio LLC")]
 [assembly: AssemblyProduct("ImportObjectsFromExcelUsing EPPlus")]
-[assembly: AssemblyCopyright("Copyright ©  2019-2020 Simio LLC")]
+[assembly: AssemblyCopyright("Copyright ©  2019-2022 Simio LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.5.4.0")]
-[assembly: AssemblyFileVersion("20.5.4.0")]
+[assembly: AssemblyVersion("22.7.4.0")]
+[assembly: AssemblyFileVersion("22.7.4.0")]
